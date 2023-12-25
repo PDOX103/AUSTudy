1e0b15b2183fe11629b1193d1d5ba2f7cce212a3
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2023-12-26 04:36:57 +0600

    change rafi

---
+++ lib/user_auth/presentation/pages/login_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Login"),
+      ),
+      body: Column(
+        children: [
+          Text("Login",style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),)
+        ],
+      ),
+    );
+  }
+}
+
