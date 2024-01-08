213bdb9143d258ff24d34002fd6d0ee962d9cff0
Md. Fahmidul Karim Rafi <137741434+PDOX103@users.noreply.github.com>
2024-01-08 15:51:22 +0600

    change

--- lib/user_auth/presentation/pages/home_page.dart
+++ lib/user_auth/presentation/pages/home_page.dart
@@ -1,80 +1,80 @@
-import 'package:firebase_auth/firebase_auth.dart';
-import 'package:flutter/cupertino.dart';
-import 'package:flutter/material.dart';
-
-import 'login_page.dart';
-
-class HomePage extends StatefulWidget {
-  const HomePage({super.key});
-
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
-
-class _HomePageState extends State<HomePage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.green,
-        centerTitle: true,
-        title: const Text(
-          'Home Page',
-          style: TextStyle(fontWeight: FontWeight.bold),
-        ),
-        shape: const RoundedRectangleBorder(
-          borderRadius: BorderRadius.only(
-            bottomLeft: Radius.circular(25),
-            bottomRight: Radius.circular(25),
-          ),
-        ),
-      ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Center(
-            child: Text(
-              "Welcome Home buddy!",
-              style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 19),
-            ),
-          ),
-          SizedBox(
-            height: 30,
-          ),
-          GestureDetector(
-            onTap: () {
-              _signOut();
-            },
-            child: Container(
-              height: 45,
-              width: 100,
-              decoration: BoxDecoration(
-                  color: Colors.green,
-                  borderRadius: BorderRadius.circular(10)),
-              child: Center(
-                child: Text(
-                  "Sign out",
-                  style: TextStyle(
-                      color: Colors.white,
-                      fontWeight: FontWeight.bold,
-                      fontSize: 18),
-                ),
-              ),
-            ),
-          )
-        ],
-      ),
-    );
-  }
-
-  void _signOut() async {
-    try {
-      await FirebaseAuth.instance.signOut();
-      Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
-      print("Successfully signed out");
-    } catch (e) {
-      print("Error during sign out: $e");
-      // Handle sign-out error if needed
-    }
-  }
-}
+// import 'package:firebase_auth/firebase_auth.dart';
+// import 'package:flutter/cupertino.dart';
+// import 'package:flutter/material.dart';
+//
+// import 'login_page.dart';
+//
+// class HomePage extends StatefulWidget {
+//   const HomePage({super.key});
+//
+//   @override
+//   State<HomePage> createState() => _HomePageState();
+// }
+//
+// class _HomePageState extends State<HomePage> {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         backgroundColor: Colors.green,
+//         centerTitle: true,
+//         title: const Text(
+//           'Home Page',
+//           style: TextStyle(fontWeight: FontWeight.bold),
+//         ),
+//         shape: const RoundedRectangleBorder(
+//           borderRadius: BorderRadius.only(
+//             bottomLeft: Radius.circular(25),
+//             bottomRight: Radius.circular(25),
+//           ),
+//         ),
+//       ),
+//       body: Column(
+//         mainAxisAlignment: MainAxisAlignment.center,
+//         children: [
+//           Center(
+//             child: Text(
+//               "Welcome Home buddy!",
+//               style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold, fontSize: 19),
+//             ),
+//           ),
+//           SizedBox(
+//             height: 30,
+//           ),
+//           GestureDetector(
+//             onTap: () {
+//               _signOut();
+//             },
+//             child: Container(
+//               height: 45,
+//               width: 100,
+//               decoration: BoxDecoration(
+//                   color: Colors.green,
+//                   borderRadius: BorderRadius.circular(10)),
+//               child: Center(
+//                 child: Text(
+//                   "Sign out",
+//                   style: TextStyle(
+//                       color: Colors.white,
+//                       fontWeight: FontWeight.bold,
+//                       fontSize: 18),
+//                 ),
+//               ),
+//             ),
+//           )
+//         ],
+//       ),
+//     );
+//   }
+//
+//   void _signOut() async {
+//     try {
+//       await FirebaseAuth.instance.signOut();
+//       Navigator.pushReplacement(context, MaterialPageRoute(builder: (context) => LoginPage()));
+//       print("Successfully signed out");
+//     } catch (e) {
+//       print("Error during sign out: $e");
+//       // Handle sign-out error if needed
+//     }
+//   }
+// }
