a0106ec339daf260dfa4f556d9899760dd0f9a47
Utsha Karmakar <utshakarmakar04109@gmail.com>
2024-01-09 13:44:50 +0600

    Delete Vedio Call directory

--- Vedio Call/main.dart
+++
@@ -1,7 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:video_conference/pages/homepage.dart';
-
-void main() {
-  runApp(GetMaterialApp(home: HomePage()));
-}
