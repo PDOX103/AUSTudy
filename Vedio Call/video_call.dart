d163c6ec130f4e9b2a6c86be2bb4b7180af2ddef
Utsha Karmakar <utshakarmakar04109@gmail.com>
2024-01-28 01:33:53 +0600

    Update video_call.dart
    video call is successfullly working with the zegocloud server

--- Vedio Call/video_call.dart
+++ Vedio Call/video_call.dart
@@ -1,66 +1,33 @@
-import 'dart:convert';
-import 'package:agora_uikit/agora_uikit.dart';
 import 'package:flutter/material.dart';
-import 'package:http/http.dart';
+import 'package:video_conference/const.dart';
+import 'package:zego_uikit_prebuilt_video_conference/zego_uikit_prebuilt_video_conference.dart';
 
-class VideoCall extends StatefulWidget {
-  String channelName = "test";
 
-  VideoCall({required this.channelName});
-  @override
-  _VideoCallState createState() => _VideoCallState();
-}
 
-class _VideoCallState extends State<VideoCall> {
-  late final AgoraClient _client;
-  bool _loading = true;
-  String tempToken = "";
+class VideoConferencePage extends StatelessWidget {
+  final String conferenceID;
 
-  @override
-  void initState() {
-    getToken();
-    super.initState();
-  }
-
-  Future<void> getToken() async {
-    String link =
-       "https://agora-node-tokenserver--utshakarmakar04.repl.co/access_token?channelName=${widget.channelName}";
-
-    Response _response = await get(Uri.parse(link));
-    Map data = jsonDecode(_response.body);
-    setState(() {
-      tempToken = data["token"];
-    });
-    _client = AgoraClient(
-        agoraConnectionData: AgoraConnectionData(
-          appId: "09ac3807ca6f4f19b52c8a6488ddd5d0",
-          tempToken:tempToken,
-          channelName: widget.channelName,
-        ),
-        enabledPermission: [Permission.camera, Permission.microphone]);
-    Future.delayed(Duration(seconds: 1)).then(
-          (value) => setState(() => _loading = false),
-    );
-  }
+  const VideoConferencePage({
+    Key? key,
+    required this.conferenceID,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-        child: _loading
-            ? Center(
-          child: CircularProgressIndicator(),
-        )
-            : Stack(
-          children: [
-            AgoraVideoViewer(
-              client: _client,
-            ),
-            AgoraVideoButtons(client: _client)
-          ],
+    return SafeArea(
+
+      child: ZegoUIKitPrebuiltVideoConference(
+        appID: appId, // Fill in the appID that you get from ZEGOCLOUD Admin Console.
+        appSign: appSign, // Fill in the appSign that you get from ZEGOCLOUD Admin Console.
+        userID: 'user_id_${DateTime.now().millisecondsSinceEpoch}',
+        userName: 'user_android_${DateTime.now().millisecondsSinceEpoch}',
+        conferenceID: conferenceID,
+        config: ZegoUIKitPrebuiltVideoConferenceConfig(
+          turnOnCameraWhenJoining: false,
+          turnOnMicrophoneWhenJoining: false,
         ),
       ),
+
     );
-    ;
   }
 }
